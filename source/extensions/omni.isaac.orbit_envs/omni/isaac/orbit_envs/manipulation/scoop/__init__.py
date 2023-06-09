04fe71be229cae3f855d035dc0b30d0e05f1b64e
AmanuelErgogo <amanuel.ergogo@gmail.com>
2023-06-09 14:42:40 +0200

    lamane

--- source/extensions/omni.isaac.orbit_envs/omni/isaac/orbit_envs/manipulation/scoop/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Environment food aqusition with spoon/fork on fixed-arm robots."""
-
-from .scoop_cfg import ScoopEnvCfg
-from .scoop_env import ScoopEnv
-
-__all__ = ["ScoopEnvCfg", "ScoopEnv"]
-
